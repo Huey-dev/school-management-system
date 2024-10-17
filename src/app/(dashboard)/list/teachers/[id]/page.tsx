9a0e3778cb018fa9ab550ed0ac6d9c2b24739fdc
Matthew Okadinya <matthewokadinya@gmail.com>
2024-10-17 12:12:38 +0100

    add performance component

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,14 +1,15 @@
 import Announcement from "@/app/components/Announcement";
 import BigCalendar from "@/app/components/Bigcalender";
+import Performance from "@/app/components/Performance";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex-1 p-4 flex flex-col xl:flex-row gap-4">
+    <div className="flex-1 p-4 flex flex-col xl:flex-row lg:flex-row gap-4">
       {/* LEFT */}
-      <div className="w-full xl:w-2/3">
+      <div className="w-full xl:w-2/3 lg:w-2/3">
         {/* TOP */}
         <div className="flex flex-col lg:flex-row gap-4">
           {/* TEACHER INFO CARD */}
@@ -58,50 +59,54 @@ const Page = () => {
             </div>
           </div>
           {/* SMALL CARDS */}
-          <div className=" flex-1 flex flex-wrap gap-4 justify-between">
+          <div className="flex-1 flex gap-4 justify-between flex-wrap">
             {/* CARDS */}
-            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleAttendance.png"
                 alt="singleattendance"
                 height={24}
                 width={24}
+                className="h-6 w-8"
               />
               <div className="">
                 <h1 className="text-xl font-semibold">90%</h1>
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
-            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleBranch.png"
                 alt="singlebranch"
                 height={24}
                 width={24}
+                className="h-6 w-8"
               />
               <div className="">
                 <h1 className="text-xl font-semibold">2</h1>
                 <span className="text-sm text-gray-400">Branches</span>
               </div>
             </div>
-            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleLesson.png"
                 alt="singlelesson"
                 height={24}
                 width={24}
+                className="h-6 w-8"
               />
               <div className="">
                 <h1 className="text-xl font-semibold">6</h1>
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
-            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <div className="bg-white p-4 gap-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleClass.png"
                 alt="singleclass"
                 height={24}
                 width={24}
+                className="h-6 w-8"
               />
               <div className="">
                 <h1 className="text-xl font-semibold">14</h1>
@@ -117,20 +122,30 @@ const Page = () => {
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+      <div className="w-full xl:w-1/3 lg:w-1/3 flex flex-col gap-4">
         {/* SHORTCUT COMPONENT */}
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-sm text-gray-400">
-            <Link className="p-3 rounded-md bg-skylight" href="/">Teacher&apos;s classes</Link>
-            <Link className="p-3 rounded-md bg-purplelight" href="/">Teacher&apos;s students</Link>
-            <Link className="p-3 rounded-md bg-yellow" href="/">Teacher&apos;s lessons</Link>
-            <Link className="p-3 rounded-md bg-orange-200" href="/">Teacher&apos;s exams</Link>
-            <Link className="p-3 rounded-md bg-pink-50" href="/">Teacher&apos;s assignments</Link>
+            <Link className="p-3 rounded-md bg-skylight" href="/">
+              Teacher&apos;s classes
+            </Link>
+            <Link className="p-3 rounded-md bg-purplelight" href="/">
+              Teacher&apos;s students
+            </Link>
+            <Link className="p-3 rounded-md bg-yellow" href="/">
+              Teacher&apos;s lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-orange-200" href="/">
+              Teacher&apos;s exams
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Teacher&apos;s assignments
+            </Link>
           </div>
         </div>
         {/* PERFORMANCE COMPONENT */}
-        <div className=""></div>
+        <Performance />
         {/* ANNOUNCEMENT */}
         <Announcement />
       </div>
